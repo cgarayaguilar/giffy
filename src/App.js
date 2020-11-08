20405cbed6771604963e09f9b89bec09a00bb6e5
Cesar Garay <cgaray.aguilar97@gmail.com>
2020-11-07 20:27:14 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState([])
+
+  return (
+    <div className="App">
+      <section className="App-content">gifs</section>
+    </div>
+  )
+}
+
+export default App
